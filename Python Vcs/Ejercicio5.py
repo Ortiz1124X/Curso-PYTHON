73f93bf6c0f26e703332e869d1ca12dc89e5db89
Ortiz1124X <124809800+Ortiz1124X@users.noreply.github.com>
2023-03-24 15:54:55 +0100

    Add files via upload

---
+++ Python Vcs/Ejercicio5.py
@@ -0,0 +1,22 @@
+
+class Alumno:
+
+    _aprueba = True
+    _deficiente = False
+
+    nombre = "Luis Felipe"
+    nota = "Su Nota es 10.0 es Excelente, Aprobado para Quimica"
+
+    nombre1 = "Laura Maria"
+    nota1 = "Su Nota es 4.0 es Deficiente, No Aprobado para Quimica"
+
+    def aprueba(self):
+        self._aprueba = True
+
+    def deficiente(self):
+        self._deficiente = False
+
+
+Estudiante = Alumno()
+print(Alumno.nombre, Alumno.nota)
+print(Alumno.nombre1, Alumno.nota1)

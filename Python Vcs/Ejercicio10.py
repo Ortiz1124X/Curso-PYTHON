95a6febd8d861466596a0a9836ab6902e387f776
Ortiz1124X <124809800+Ortiz1124X@users.noreply.github.com>
2023-04-04 23:25:13 +0200

    Add files via upload

---
+++ Python Vcs/Ejercicio10.py
@@ -0,0 +1,8 @@
+
+listaPaises = {'Colombia', 'Colombia', 'Brasil', 'Puerto Rico', 'Belgica', 'Honduras', 'España'}
+country = sorted(listaPaises)
+print(country)
+
+for e in listaPaises:
+    print(e)
+    
